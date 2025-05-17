03a229736109fde496412d517ab78b6d30b5a449
Goodymind <aabuke9@gmail.com>
2025-05-17 16:42:35 +0800

    project react setup

--- jollibee_minmaxer/settings.py
+++ jollibee_minmaxer/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "core",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "jollibee_minmaxer.urls"
