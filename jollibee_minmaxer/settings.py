ccd102085919731e3ec7ef12c5002de051b79352
Goodymind <aabuke9@gmail.com>
2025-05-17 17:30:28 +0800

    add corsheader in settings.py

--- jollibee_minmaxer/settings.py
+++ jollibee_minmaxer/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# rest framework dict
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
